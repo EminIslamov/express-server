a3b78afd85f9be4963d0c0fcfd5db9f8138c9aab
emin <Magamedi98@mail.ru>
2021-07-22 10:13:49 +0300

    first commit

---
+++ controllers/products.controller.js
@@ -0,0 +1,5 @@
+module.exports.productsController = {
+  getProducts: (req, res) => {
+     res.json([1,2,3])
+  }
+}
---
+++ routes/products.route.js
@@ -0,0 +1,9 @@
+const { Router } = require('express');
+
+const { productsController } = require('../controllers/products.controller');
+
+const router = Router();
+
+router.get('/products', productsController.getProducts);
+
+module.exports = router;
